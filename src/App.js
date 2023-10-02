55672c343a016e7555aaa40bce1a0a28971fad5b
Suman Subedi <ssubedi60@gmail.com>
2023-10-01 23:27:08 -0500

    added stats feature

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ export default function App() {
         onDeleteItems={handleDeleteItems}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -122,10 +122,24 @@ function Item({ item, onDeleteItems, onToggleItem }) {
 
 // STATS COMPONENT
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding items to your packing list ✅ </em>
+      </p>
+    );
+  const numOfItem = items.length;
+  const numOfPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numOfPacked / numOfItem) * 100);
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️ "
+          : `You have ${numOfItem} items on your list, and you already packed
+        ${numOfPacked} (${percentage}%) `}
+      </em>
     </footer>
   );
 }
